a593d360fb8f7ced76d0f7a59ff6c7b2fe9bf31e
Shreyans Jaiswal <146889792+Shreyans-704@users.noreply.github.com>
2025-08-24 15:09:15 +0530

    supabse file name changed

--- client/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://gfzfalbtebsvsfnwmoqj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmemZhbGJ0ZWJzdnNmbndtb3FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MTE2NzksImV4cCI6MjA3MTA4NzY3OX0.84u5OjKQjvy8nnt1rYrOjYq41Z5OqPkM-6cbSqMgLfY'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
